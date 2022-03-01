8fca02c95e7b6341166163bff235ff61c15b3c49
Guneet <guneet719@outlook.com>
2022-03-01 23:20:23 +0530

    Added New Components

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+const justinListItems = ["New Seasonal Favorites",
+                         "New to the Market",
+                         "Breadwinners",
+                         "Celebrating 10 Years Together",
+                         "Takeout at Home",
+                         "Keen on Green",
+                         "Fresh Catch",
+                         "St. Patrick's Day"
+                        ]
+
+// const testElement = document.querySelector(
+//     ".header-item-div-margin-left"
+//     ).querySelectorAll('.header-item-div-link__text');
+
+const testElement= document.querySelectorAll('.dropdown');
+
+for(var i=0; i<testElement.length; i++) {
+    testElement[i].addEventListener("mouseover", function(){
+    })
+}

20b7a61c32887ca1443694495a8e55fc57826992
Guneet <guneet719@outlook.com>
2022-03-03 23:45:32 +0530

    Added Dropdown

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 
+
 const justinListItems = ["New Seasonal Favorites",
                          "New to the Market",
                          "Breadwinners",
@@ -15,8 +16,22 @@ const justinListItems = ["New Seasonal Favorites",
 //     ).querySelectorAll('.header-item-div-link__text');
 
 const testElement= document.querySelectorAll('.dropdown');
+const testElement1= document.querySelectorAll('.header-item-div-container');
+
+
 
 for(var i=0; i<testElement.length; i++) {
-    testElement[i].addEventListener("mouseover", function(){
+    testElement[i].style.display = 'none';
+}
+
+testElement[0].style.display = 'block';
+
+
+for(var i=0; i<testElement1.length; i++) {
+    testElement1[i].addEventListener("mouseover", function(evt){
+        // this.children[1].style.display = 'block';
+    })
+    testElement1[i].addEventListener("mouseout", function(evt){
+        // this.children[1].style.display = 'none';
     })
 }

2bd9fa1be2a3831b91df5461f1b3911d620fa41c
Guneet <guneet719@outlook.com>
2022-03-09 23:42:48 +0530

    Added Dropdown Items

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 
 
 
-const justinListItems = ["New Seasonal Favorites",
-                         "New to the Market",
-                         "Breadwinners",
-                         "Celebrating 10 Years Together",
-                         "Takeout at Home",
-                         "Keen on Green",
-                         "Fresh Catch",
-                         "St. Patrick's Day"
-                        ]
+const justinListItems = ["New Seasonal Favorites", "New to the Market", "Breadwinners", "Celebrating 10 Years Together", "Takeout at Home", "Keen on Green", "Fresh Catch", "St. Patrick's Day"]
 
-// const testElement = document.querySelector(
-//     ".header-item-div-margin-left"
-//     ).querySelectorAll('.header-item-div-link__text');
+const mealKits = ["Main Course", "Special Diets", "Celebrations", "Appetizers", "Boards & Platters", "Side Dishes", "Side Salads", "Quick & Easy Lunch", "Desserts", "Cooking With Kids", "One-Pot Cooking", "Family Favorites", "Cocktails"]
+
+const produce = ["New & Peak Season", "CSA Boxes", "Prepped Fruit", "Prepped Vegetables", "Flowers", "Fruits", "Avocados", "Vegetables", "Greens", "Mushrooms", "Potatoes & Beets", "Garlic & Onions", "Herbs", "Bulk Produce", "Frozen Fruit & Vegetables"]
+
+const listItems = [justinListItems, mealKits, produce]
+
 
 const dropdown = document.querySelectorAll('.dropdown');
 const dropdownActivator = document.querySelectorAll('.header-item-div-container');
-const dropdownDiv = document.querySelector('.dropdown');
+const dropdownDiv = document.querySelectorAll('.dropdown');
 
-for(var i=0; i<justinListItems.length; i++) {
-    var li = document.createElement('li');
-    li.innerHTML = justinListItems[i];
-    li.classList.add("dropdown-container__li");
-    dropdownDiv.appendChild(li);
+const addListElements = (List, divIndex) => {
+        console.log(List);
+        for(var i=0; i<List.length; i++) {
+            var li = document.createElement('li');
+            li.innerHTML = List[i];
+            li.classList.add("dropdown-container__li");
+            dropdownDiv[divIndex].appendChild(li);
+        }
 }
+
+for(var i=0; i<3; i++) {
+    addListElements(listItems[i], i);
+}
+
 
 for(var i=0; i<dropdown.length; i++) {
     dropdown[i].style.display = 'none';
